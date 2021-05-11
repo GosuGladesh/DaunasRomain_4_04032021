ae8db557f407c69c0d28bbf8bb6b03474c2ea0a4
Romain DAUNAS <romain.daunas@gmail.com>
2021-05-11 15:33:12 +0200

    form validation wrapup

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -44,47 +44,78 @@ const form = document.getElementsByName("reserve");
 function firstNameValidation() {
   if(firstName.value.length > 2){
       firstName.insertAdjacentHTML('afterend','<p>Veuillez entrer 2 caractères ou plus pour le champ du prénom.</p>');
+      return false;
   }
+  return true;
 }
 function lastNameValidation() {
   if(lastName.value.length > 2){
       lastName.insertAdjacentHTML('afterend','<p>Veuillez entrer 2 caractères ou plus pour le champ du nom.</p>');
+      return false;
   }
+  return true;
 }
 function birthdateValidation() {
   if(birthdate.value == ""){
       birthdate.insertAdjacentHTML('afterend','<p>Vous devez entrer votre date de naissance.</p>');
+      return false;
   }
+  return true;
 }
 function emailValidation() {
   if(email.value.includes("@") == false){
     email.insertAdjacentHTML('afterend','<p>Vous devez entrer un email valide</p>');
+    return false;
   }
+  return true;
 }
 function quantityValidation() {
   if( isNaN(parseInt(quantity.value))){
     quantity.insertAdjacentHTML('afterend','<p>Vous devez entrer un nombre</p>');
+    return false;
   }
+  return true;
 }
 function cguValidation() {
   if(cgu.checked == false){
     cgu.insertAdjacentHTML('afterend', '<p>Vous devez vérifier que vous acceptez les termes et conditions.</p>');
+    return false;
   }    
+  return true;
 }
 function locationValidation() {
   let locations = document.querySelectorAll('input[type=radio]');  
   for (let location of locations) {
     if(location.checked) {
-      return;
+      return true;
     }
   }
   let locationElement = document.getElementById("location6")
   locationElement.insertAdjacentHTML('afterend', '<p>Vous devez choisir une option.</p>');
+  return false;
 }
-  
+
+function formValidation() {
+
+    if(
+      firstNameValidation() &&
+      lastNameValidation() &&
+      emailValidation() &&
+      birthdateValidation() &&
+      quantityValidation() &&
+      locationValidation() &&
+      cguValidation()
+      ){
+        return true;
+      }
+      return false;
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-
+  if(formValidation()) {
+    form.style.display = "none";
+    form.insertAdjacentHTML('beforestart','<p>Merci ! Votre réservation a été reçue.</p>')
+  }
 })
 
